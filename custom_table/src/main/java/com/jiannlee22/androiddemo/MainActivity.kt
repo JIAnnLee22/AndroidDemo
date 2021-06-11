953d5cbaf292eaea0a54d8850fa816f17bc53cb0
JIAnnLee22 <947807614@qq.com>
2021-06-11 14:38:52 +0800

    navigation demo

--- custom_table/src/main/java/com/jiannlee22/androiddemo/MainActivity.kt
+++ custom_table/src/main/java/com/jiannlee22/androiddemo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jiannlee22.androiddemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayout
+import com.jiannlee22.androiddemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //视图绑定
+    val mBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
+        //第一列相同合并
+        //第二列相同并且第一列相同,合并
     }
 }
---
+++ navigationdemo/src/main/java/com/jiannlee22/navigationdemo/fragment/FragmentD.kt
@@ -0,0 +1,42 @@
+package com.jiannlee22.navigationdemo.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
+import com.jiannlee22.navigationdemo.R
+import com.jiannlee22.navigationdemo.databinding.FragmentCommonBinding
+
+/**
+ * Created by JIAnnLee22 on 2021/06/08 19:19
+ * 说明:
+ */
+class FragmentD : Fragment() {
+    private lateinit var rootView: ViewBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        rootView = FragmentCommonBinding.inflate(inflater, container, false)
+        initView(container)
+        return rootView.root
+    }
+
+    private fun initView(container: ViewGroup?) {
+        val text = TextView(requireContext())
+        text.text = "FragmentD"
+        val buttonNext = Button(requireContext())
+        container?.addView(text)
+        container?.addView(buttonNext)
+        buttonNext.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentD_to_fragmentA)
+        }
+    }
+
+}
