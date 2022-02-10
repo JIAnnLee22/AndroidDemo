10d703c35db37ccc59f58e3d00f129ff321b99d7
JIAnnLee22 <947807614@qq.com>
2022-02-10 10:44:05 +0800

    添加简单的通知

--- notificationdemo/src/main/java/com/jiannlee22/notificationdemo/NotificationActivity.kt
+++ notificationdemo/src/main/java/com/jiannlee22/notificationdemo/NotificationActivity.kt
@@ -1,11 +1,28 @@
 package com.jiannlee22.notificationdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+        val builder = NotificationCompat.Builder(this, "TEST_NOTIFICATION")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Much longer text that cannot fit one line...")
+            )
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+        with(NotificationManagerCompat.from(this)) {
+            notify(0, builder.build())
+        }
     }
 }
